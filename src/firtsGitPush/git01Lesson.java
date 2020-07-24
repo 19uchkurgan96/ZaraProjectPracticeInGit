434119611fad45450f932094c56d6818b06184b5
19uchkurgan96 <19zarnigor96@gmail.com>
2020-07-24 10:35:39 -0700

    add  git01Lesson.java

---
+++ src/firtsGitPush/git01Lesson.java
@@ -0,0 +1,9 @@
+package firtsGitPush;
+
+public class git01Lesson {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Batch 18 team ");
+        System.out.println("Hi evryone");
+    }
+}
